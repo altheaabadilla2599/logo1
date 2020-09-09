a4dcd9cf94126fe5c2cf4dffbff64dda0e8fb609
altheaabadilla2599 <70379536+altheaabadilla2599@users.noreply.github.com>
2020-09-09 10:27:55 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" type="text/css" href="css/cake.css">
+<title>Logo1</title>
+
+
+<div class="logoholder"> 
+ 
+  </div>
+
+
+
+<div id="first-layer">
+
+  <div class="frosting1"></div>
+     <div class="frosting11" ></div>
+	 <div class="frosting111" ></div>
+	 <div class="frosting1111" ></div>
+	 <div class="frosting0" ></div>
+	  <div class="frosting00" ></div>
+	   <div class="frosting000" ></div>
+	
+</div>
+<div id="second-layer">
+  <div class="frosting2" id="frosting2"></div>
+</div>
+  <div id="last-layer">
+    <div class="frosting3" id="frosting3"></div>
+ </div>
+ 
+ 
